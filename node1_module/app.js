9b3a4b9b3c17185d7b86ade61353f7f3b30451ee
GXiaoxia <32625822+GXiaoxia@users.noreply.github.com>
2017-10-20 08:08:02 -0500

    Update app.js

--- node1_module/app.js
+++ node1_module/app.js
@@ -1,4 +1,4 @@
-var stuff = require("./module");	// 不需要加.js
+var stuff = require("./module");	// 不需要加.js, ./表示当前目录
 console.log(stuff.counter(['1','2','3']));
 console.log(stuff.adder(1,2));
 console.log(stuff.pi);
