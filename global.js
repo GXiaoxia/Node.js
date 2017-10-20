530b5e79c24a5c17c5f0bd1a44db874c5e47f444
GXiaoxia <1195200288@qq.com>
2017-10-20 19:11:33 +0800

    add file

---
+++ global.js
@@ -0,0 +1,20 @@
+// console.log('hello NodeJS');
+
+// setTimeout(function(){
+// 	console.log('3 seconds have passed');
+// },3000);
+
+/*var time = 0;
+var timer = setInterval(function(){
+	time += 2;
+	if(time > 5){
+		clearInterval(timer);
+	}else{
+		console.log(time + '2 seconds have passed');
+	}
+	
+},2000);*/
+
+console.log(__dirname);		// 当前执行脚本所在的目录
+console.log(__filename);	// 当前正在执行的脚本的文件名
+
