5649c382bf7fb0ddb09101581619c6535f0799e1
GXiaoxia <32625822+GXiaoxia@users.noreply.github.com>
2017-10-21 05:08:05 -0500

    Update filesystem.js

--- node2_fs/filesystem.js
+++ node2_fs/filesystem.js
@@ -35,12 +35,32 @@ fs.writeFile("writeMe.txt","12345",function(err,data){
 });*/
 
 // 创建文件夹
-fs.mkdir("stuff",function(){
+/*fs.mkdir("stuff",function(){
 
-});
+});*/
 
 // 删除文件夹
-fs.rmdir("stuff",function(){
+/*fs.rmdir("stuff",function(){
 	
-});
+});*/
+
+
+
+var fs = require('fs');
+
+// 异步asynchronous
+fs.readFile(path,[,options],callback);				// 读取文件
+fs.open(path,flags[,mode],callback)  				// 打开文件,flags-文件打开的行为(r,r+) 
+fs.stat(path,callback);								// 获取文件信息,callback(err,stats)
+fs.writeFile(file,data[,options],callback);			// 写入文件,callback(err)
+fs.read(fd,buffer,offset,length,position,callback);		// 读取文件
+fs.close(fd,callback);								// 关闭文件,fd-通过fs.open()方法返回的文件描述符,callback(err)
+fs.unlink(path,callback);							// 删除文件,callback无参数
+
+
+// 同步synchronous
+fs.readFileSync(path[,options]);
+fs.openSync(path,flags[,mode]);
+fs.writeFile(file,data[,options]);
+
 
