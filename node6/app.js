905e4e1d8b0d9831b3d51eeabeba8c8f65451592
GXiaoxia <32625822+GXiaoxia@users.noreply.github.com>
2017-10-26 06:22:41 -0500

    Update app.js

--- node6/app.js
+++ node6/app.js
@@ -32,3 +32,13 @@ app.listen(3000);		// 第二个参数默认localhost
 	5.cnpm install express --save
 	6.nodemon app	            // 运行    html文件需要写rs     
 */
+/*
+Express路由:
+	由一个URL和一个特定的HTTP方法(get,post等)组成的,涉及到应用如何响应客户端对某个网站节点的访问.
+	每一个路由都可以有一个或多个处理器函数,当匹配到路由时,这个/些函数将被执行.
+路由的定义结构:    app.METHOD(path,[callback..],callback)
+	app是express对象的一个实例,METHOD是一个HTTP请求方法,path是服务器上的路径,callback是当路由匹配时要执行的函数
+	express定义了和HTTP请求对应的路由方法:
+	get,post,put,head,delete,options,trace,copy等
+
+*/
